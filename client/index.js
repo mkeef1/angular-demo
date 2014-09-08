549fd5d79ef689567d6fd2c5bb3887e11fddfefd
matt keef <mkeef1@gmail.com>
2014-09-08 14:00:06 -0500

    demo done

--- client/index.js
+++ client/index.js
@@ -4,5 +4,11 @@
   angular.module('demo', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.name = 'Groot!';
+    $scope.age = 6;
+    $scope.dogs = [
+      {name:'fido', age:3},
+      {name:'baxter', age:5},
+      {name:'flufy', age:7}
+    ];
   }]);
 })();
